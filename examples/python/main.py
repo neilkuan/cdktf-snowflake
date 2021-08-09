6176be3f6cfc1a1361c9a042850bf486ffa02e7f
Bryan Galvin <bcgalvin@gmail.com>
2021-08-08 23:49:50 -0700

    add python example

---
+++ examples/python/main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import os
+
+from cdktf import App, TerraformStack
+from cdktf_snowflake import SnowflakeProvider, User
+from constructs import Construct
+
+
+class SnowflakeStack(TerraformStack):
+    def __init__(self, scope: Construct, ns: str):
+        super().__init__(scope, ns)
+        config = SnowflakeProvider(
+            self,
+            id="provider",
+            account=os.environ.get("SNOWFLAKE_ACCOUNT"),
+            password=os.environ.get("SNOWFLAKE_PASSWORD"),
+            username=os.environ.get("SNOWFLAKE_USERNAME"),
+            role=os.environ.get("SNOWFLAKE_ROLE"),
+        )
+        test_user = User(self, "test-user", name="test", provider=config)
+
+
+app = App()
+SnowflakeStack(app, "py-cdktf-snowflake")
+
+app.synth()
